f0177b83eb784c2670536b3b1e0d6a3e804cea40
Seungil Kim <ksi990302@gmail.com>
2023-06-21 04:03:51 +0900

    Feat: access, refresh token strategy 추가

---
+++ src/auth/strategies/access-token.strategy.ts
@@ -0,0 +1,18 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(private configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.get<string>('JWT_ACCESS_TOKEN_SECRET'),
+    });
+  }
+
+  validate(payload: any) {
+    return payload;
+  }
+}
