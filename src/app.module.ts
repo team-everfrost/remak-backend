9b97d5c919242ee0977ca1052e647a03d45fe53c
Seungil Kim <ksi990302@gmail.com>
2023-06-21 00:06:31 +0900

    Feat: prisma module, service 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, ConfigModule.forRoot({ isGlobal: true }), AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
