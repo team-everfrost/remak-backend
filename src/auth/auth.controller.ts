5c7b11fd7ab708269697a8c6cb46544580f870f2
ks1ksi <ksi990302@gmail.com>
2023-06-23 02:54:04 +0900

    ✨ Feat: auth controller http code 추가
    created, ok
    
    #

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignupDto } from './dto/signup.dto';
 import { Tokens } from './types/tokens.type';
@@ -9,21 +9,25 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/local/signup')
+  @HttpCode(HttpStatus.CREATED)
   signupLocal(@Body() signupDto: SignupDto): Promise<Tokens> {
     return this.authService.signupLocal(signupDto);
   }
 
   @Post('/local/login')
+  @HttpCode(HttpStatus.OK)
   loginLocal(@Body() authDto: AuthDto): Promise<Tokens> {
     return this.authService.loginLocal(authDto);
   }
 
   @Post('/logout')
+  @HttpCode(HttpStatus.OK)
   logout() {
     return this.authService.logout();
   }
 
   @Post('/refresh')
+  @HttpCode(HttpStatus.OK)
   refresh() {
     return this.authService.refreshTokens();
   }
