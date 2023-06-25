a485e15648cce5f121bd322b8a69bfa0794b4a5b
ks1ksi <ksi990302@gmail.com>
2023-06-25 14:04:27 +0900

    ✨ Feat: swagger 추가
    #

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   (BigInt.prototype as any).toJSON = function () {
     return this.toString();
   };
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Remak')
+    .setDescription('Remak API description')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
