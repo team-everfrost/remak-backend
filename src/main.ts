ea5cab45ca0cc45eafe6b33c3cef22a4cc535d13
ks1ksi <ksi990302@gmail.com>
2023-06-26 18:28:24 +0900

    ✨ Feat: swagger에 auth 기능 추가
    #

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
     .setTitle('Remak')
     .setDescription('Remak API description')
     .setVersion('0.1')
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
