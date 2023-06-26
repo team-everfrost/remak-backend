4b44a9909747bf50730e5ab6d8f0aa7fce357de8
ks1ksi <ksi990302@gmail.com>
2023-06-27 01:30:19 +0900

    ✨ Feat: validation whitelist 추가
    불필요한 프로퍼티 필터링. dto에 정의되지 않은 프로퍼티가 전달될 경우, 자동으로 그 프로퍼티를 제거한다.
    
    #

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
     SwaggerModule.setup('api', app, document);
   }
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
 }
 
