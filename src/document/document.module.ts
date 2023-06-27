e1b20b237c1ac01c59e33fb6f14d7d0abcc7036c
ks1ksi <ksi990302@gmail.com>
2023-06-28 02:55:30 +0900

    🌀 Refactor: jwt module 제거
    #

--- src/document/document.module.ts
+++ src/document/document.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
 import { DocumentController } from './document.controller';
 import { DocumentService } from './document.service';
 
 @Module({
-  imports: [JwtModule],
   controllers: [DocumentController],
   providers: [DocumentService],
 })
