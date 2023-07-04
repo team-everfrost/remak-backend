74cbf46506b1df9a44eb2302706ae657061e7e1f
ks1ksi <ksi990302@gmail.com>
2023-07-04 14:38:04 +0900

    ✨ Feat: cors, port 설정 추가
    #

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -7,8 +8,21 @@ async function bootstrap() {
   (BigInt.prototype as any).toJSON = function () {
     return this.toString();
   };
+
   const app = await NestFactory.create(AppModule);
 
+  const configService = app.get(ConfigService);
+
+  // CORS
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
+  // Swagger
   if (process.env.NODE_ENV !== 'production') {
     const config = new DocumentBuilder()
       .setTitle('Remak')
@@ -21,8 +35,11 @@ async function bootstrap() {
     SwaggerModule.setup('api', app, document);
   }
 
+  // Pipes
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(3000);
+
+  const port = configService.get<number>('PORT', 3000);
+  await app.listen(port);
 }
 
 bootstrap();
