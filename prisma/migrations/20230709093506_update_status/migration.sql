11431f97a8282508043fc6d667df9785ec85871c
qilip <qilip@qilip.io>
2023-07-10 16:56:59 +0900

    💥 Break: Status 마이그레이션 쿼리 추가
    #

--- prisma/migrations/20230709093506_update_status/migration.sql
+++ prisma/migrations/20230709093506_update_status/migration.sql
@@ -7,6 +7,11 @@
 -- AlterEnum
 BEGIN;
 CREATE TYPE "Status_new" AS ENUM ('SCRAPE_PENDING', 'SCRAPE_PROCESSING', 'SCRAPE_REJECTED', 'EMBED_PENDING', 'EMBED_PROCESSING', 'EMBED_REJECTED', 'COMPLETED');
+
+-- update existing values
+UPDATE "document" SET "status" = 'EMBED_PENDING' WHERE "status" = 'PENDING';
+-- continue for all other values...
+
 ALTER TABLE "document" ALTER COLUMN "status" DROP DEFAULT;
 ALTER TABLE "document" ALTER COLUMN "status" TYPE "Status_new" USING ("status"::text::"Status_new");
 ALTER TYPE "Status" RENAME TO "Status_old";
@@ -14,6 +19,3 @@ ALTER TYPE "Status_new" RENAME TO "Status";
 DROP TYPE "Status_old";
 ALTER TABLE "document" ALTER COLUMN "status" SET DEFAULT 'SCRAPE_PENDING';
 COMMIT;
-
--- AlterTable
-ALTER TABLE "document" ALTER COLUMN "status" SET DEFAULT 'SCRAPE_PENDING';
