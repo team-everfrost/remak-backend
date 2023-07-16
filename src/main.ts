58252a7275f27f411555e8464622ab295c9e65b3
Seungil Kim <ksi990302@gmail.com>
2023-07-16 16:43:59 +0900

    ⚡️ Update: swagger path 변경
    api에서 docs로 변경
    
    #

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,7 @@ async function bootstrap() {
       .build();
 
     const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
+    SwaggerModule.setup('docs', app, document);
   }
 
   // Pipes
