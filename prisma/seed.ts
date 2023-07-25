665763c1dee24c3598bfc5563b688f6a34187bf7
Seungil Kim <ksi990302@gmail.com>
2023-07-26 01:54:04 +0900

    ⚡️ Update: 문서 여러 개 생성하도록 seed 수정
    #

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,36 +5,30 @@ import {
   Role,
   Status,
 } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
+const password = bcrypt.hashSync('password', 10);
 
-async function createUserWithDocumentsAndTags(index: number) {
+async function createUserWithDocumentsAndTags(index: number, numDocs: number) {
+  const documentData = Array.from({ length: numDocs }, (_, i) => ({
+    title: `user${index} document${i + 1}`,
+    type: DocumentType.MEMO,
+    url: `http://example.com/document${index}${i + 1}`,
+    content: `This is user${index} document${i + 1}`,
+    summary: `user${index} document${index}${i + 1} summary`,
+    status: Status.EMBED_PENDING,
+  }));
+
   const createdUser = await prisma.user.create({
     data: {
-      email: `email${index}@email.com`,
-      password: 'password',
+      email: `example${index}@example.com`,
+      password: password,
       name: `name${index}`,
       imageUrl: `http://example.com/image${index}.jpg`,
       role: Role.BASIC,
       documents: {
-        create: [
-          {
-            title: `user${index} document1`,
-            type: DocumentType.MEMO,
-            url: `http://example.com/document${index}`,
-            content: `This is user${index} document1`,
-            summary: `user${index} document${index} summary`,
-            status: Status.EMBED_PENDING,
-          },
-          {
-            title: `user${index} document2`,
-            type: DocumentType.WEBPAGE,
-            url: `http://example.com/document${index}`,
-            content: `This is user${index} document2`,
-            summary: `user${index} document${index} summary`,
-            status: Status.EMBED_PENDING,
-          },
-        ],
+        create: documentData,
       },
     },
     include: {
@@ -71,15 +65,23 @@ async function createUserWithDocumentsAndTags(index: number) {
 
 async function createEmbeddedText(documentId: bigint, uid: string) {
   const vec = Array.from({ length: 1536 }, () => Math.random());
-  const vector = JSON.stringify(vec);
-  await prisma.$executeRaw`INSERT INTO embedded_text (document_id, uid, vector)
-                           VALUES (${documentId}, ${uid}, ${vector}::vector)`;
+  await prisma.$executeRaw`insert into embedded_text (document_id, uid, vector)
+                             values (${documentId}, ${uid}, ${JSON.stringify(
+    vec,
+  )}::vector)`;
+
+  // 중복 문서를 검색하지 않는 기능을 테스트하기 위해 비슷한 벡터를 만듦
+  // 비슷한 벡터를 만들기 위해 0.5보다 작은 값은 0.1을 더해줌
+  await prisma.$executeRaw`insert into embedded_text (document_id, uid, vector)
+                             values (${documentId}, ${uid}, ${JSON.stringify(
+    vec.map((v) => (v < 0.5 ? v + 0.1 : v)),
+  )}::vector)`;
 }
 
 async function main() {
   for (let i = 0; i < 10; i++) {
     try {
-      const user = await createUserWithDocumentsAndTags(i);
+      const user = await createUserWithDocumentsAndTags(i, 100);
       const docs: Document[] = user.documents;
       await Promise.all(
         docs.map((doc) => createEmbeddedText(doc.id, user.uid)),
