076901b669082914398be16e2d9c1c8a3edf4d28
Seungil Kim <ksi990302@gmail.com>
2023-07-27 13:45:26 +0900

    ✨ Feat: openai 모듈, 서비스 추가
    검색어 쿼리 임베딩
    
    #

---
+++ src/openai/open-ai.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Configuration, OpenAIApi } from 'openai';
+
+@Injectable()
+export class OpenAiService {
+  private readonly openAiConfig;
+  private readonly openAi;
+  private logger = new Logger(OpenAiService.name);
+
+  constructor(private readonly configService: ConfigService) {
+    this.openAiConfig = new Configuration({
+      apiKey: configService.get<string>('OPENAI_API_KEY'),
+    });
+    this.openAi = new OpenAIApi(this.openAiConfig);
+  }
+
+  async getEmbedding(text: string): Promise<number[]> {
+    try {
+      const result = await this.openAi.createEmbedding({
+        model: 'text-embedding-ada-002',
+        input: text,
+      });
+      return result.data.data[0].embedding;
+    } catch (e) {
+      if (e.response) {
+        this.logger.error(e.response.status);
+        this.logger.error(e.response.data);
+      } else {
+        this.logger.error(e);
+      }
+      throw e;
+    }
+  }
+}
