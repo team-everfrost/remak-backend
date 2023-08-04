9f137d250a8b8ac3c2582893a15caf97a87dcafc
Seungil Kim <ksi990302@gmail.com>
2023-08-04 17:49:21 +0900

    ✨ Feat: prisma query logging 시 formatting 및 params, duration 볼 수 있도록
    #

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,12 +1,20 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import {
+  Injectable,
+  Logger,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
+import { format } from 'sql-formatter';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  private readonly logger: Logger = new Logger(PrismaService.name);
+
   constructor(private readonly configService: ConfigService) {
     super({
       datasources: {
@@ -14,7 +22,36 @@ export class PrismaService
           url: configService.get<string>('DATABASE_URL'),
         },
       },
-      log: ['query', 'info', 'warn', 'error'],
+      log: [
+        {
+          emit: 'event',
+          level: 'query',
+        },
+        {
+          emit: 'stdout',
+          level: 'error',
+        },
+        {
+          emit: 'stdout',
+          level: 'info',
+        },
+        {
+          emit: 'stdout',
+          level: 'warn',
+        },
+      ],
+    });
+    (
+      this.$on as (
+        eventType: 'query',
+        callback: (e: Prisma.QueryEvent) => void,
+      ) => void
+    )('query', (e: Prisma.QueryEvent) => {
+      this.logger.log(
+        `Query: \n${format(e.query, { language: 'postgresql' })}`,
+      );
+      this.logger.verbose(`Params: ${e.params}`);
+      this.logger.debug(`Duration: ${e.duration}ms`);
     });
   }
 
