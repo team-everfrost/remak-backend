651542fa9f68f4020cbde940cef1ef5986a2dced
Seungil Kim <ksi990302@gmail.com>
2023-08-10 23:08:52 +0900

    ✨ Feat: 해당 유저의 태그 조회 API 추가
    #

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,11 +1,17 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { TagService } from './tag.service';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiBearerAuth } from '@nestjs/swagger';
+import { GetUid } from '../decorators/get-uid.decorator';
 
 @Controller('tag')
 @UseGuards(AuthGuard('jwt'))
 @ApiBearerAuth()
 export class TagController {
   constructor(private readonly tagService: TagService) {}
+
+  @Get()
+  async getTags(@GetUid() uid: string): Promise<string[]> {
+    return await this.tagService.getTags(uid);
+  }
 }
--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -4,4 +4,12 @@ import { PrismaService } from '../prisma/prisma.service';
 @Injectable()
 export class TagService {
   constructor(private readonly prisma: PrismaService) {}
+
+  async getTags(uid: string): Promise<string[]> {
+    const tags = await this.prisma.tag.findMany({
+      where: { user: { uid } },
+    });
+
+    return tags.map((tag) => tag.name);
+  }
 }
