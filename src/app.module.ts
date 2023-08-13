a5253a62c78741a3594453c342c1906d44feb7c0
Seungil Kim <ksi990302@gmail.com>
2023-08-13 17:46:05 +0900

    🍎 Chore: MailerModule 대신 AwsModule 사용하도록 변경
    #

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
-import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -15,24 +14,6 @@ import { TagModule } from './tag/tag.module';
     PrismaModule,
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
-    MailerModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        transport: {
-          host: configService.get<string>('MAIL_HOST'),
-          port: configService.get<number>('MAIL_PORT'),
-          secure: false,
-          auth: {
-            user: configService.get<string>('MAIL_USER'),
-            pass: configService.get<string>('MAIL_PASS'),
-          },
-        },
-        defaults: {
-          from: configService.get<string>('MAIL_FROM'),
-        },
-      }),
-    }),
     DocumentModule,
     UserModule,
     TagModule,
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,12 @@
-import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { AccessTokenStrategy } from './strategies/access-token.strategy';
+import { AwsModule } from '../aws/aws.module';
 
 @Module({
-  imports: [JwtModule.register({}), MailerModule],
+  imports: [JwtModule.register({}), AwsModule],
   controllers: [AuthController],
   providers: [AuthService, AccessTokenStrategy],
 })
