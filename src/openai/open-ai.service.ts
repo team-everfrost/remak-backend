d817b6af0c76c3d5ca240df649c6a8d463905fa9
Seungil Kim <ksi990302@gmail.com>
2023-09-07 17:23:22 +0900

    🍎 Chore: openai v4로 버전업
    #

--- src/openai/open-ai.service.ts
+++ src/openai/open-ai.service.ts
@@ -1,35 +1,36 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { Configuration, OpenAIApi } from 'openai';
+import OpenAI from 'openai';
 
 @Injectable()
 export class OpenAiService {
-  private readonly openAiConfig: Configuration;
-  private readonly openAi: OpenAIApi;
+  private readonly openai: OpenAI;
   private logger = new Logger(OpenAiService.name);
 
   constructor(private readonly configService: ConfigService) {
-    this.openAiConfig = new Configuration({
+    this.openai = new OpenAI({
       apiKey: configService.get<string>('OPENAI_API_KEY'),
     });
-    this.openAi = new OpenAIApi(this.openAiConfig);
   }
 
   async getEmbedding(text: string): Promise<number[]> {
     try {
-      const result = await this.openAi.createEmbedding({
+      const result = await this.openai.embeddings.create({
         model: 'text-embedding-ada-002',
         input: text,
       });
-      return result.data.data[0].embedding;
-    } catch (e) {
-      if (e.response) {
-        this.logger.error(e.response.status);
-        this.logger.error(e.response.data);
+
+      return result.data[0].embedding;
+    } catch (error) {
+      if (error.response) {
+        this.logger.error(error.response.status); // e.g. 401
+        this.logger.error(error.response.data.message); // e.g. The authentication token you passed was invalid...
+        this.logger.error(error.response.data.code); // e.g. 'invalid_api_key'
+        this.logger.error(error.response.data.type); // e.g. 'invalid_request_error'
       } else {
-        this.logger.error(e);
+        this.logger.error(error);
       }
-      throw e;
+      throw error;
     }
   }
 }
