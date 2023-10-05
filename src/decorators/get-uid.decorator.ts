bff2ceb6ec4bce9fc398145a4650d1552d2e614d
Seungil Kim <ksi990302@gmail.com>
2023-10-05 12:20:09 +0900

    ⚡️ Update: JWT 존재하지 않는 경우 적절한 오류 반환하도록 수정
    #

--- src/decorators/get-uid.decorator.ts
+++ src/decorators/get-uid.decorator.ts
@@ -1,8 +1,17 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import {
+  createParamDecorator,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
 
 export const GetUid = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
+
+    if (!request.user || !request.user.aud) {
+      throw new UnauthorizedException('JWT token is missing or invalid.');
+    }
+
     return request.user.aud;
   },
 );
