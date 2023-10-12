49985f9d215ab89a41a3b45eb8c9bdba1b26ecea
Seungil Kim <ksi990302@gmail.com>
2023-10-13 04:11:32 +0900

    ✨ Feat: 해당 사용자가 저장한 파일 크기 및 추가로 저장할 수 있는지 여부 확인
    #

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,7 @@ import {
   HttpException,
   HttpStatus,
   Injectable,
+  Logger,
   NotFoundException,
 } from '@nestjs/common';
 import { User } from '@prisma/client';
@@ -11,6 +12,10 @@ import { UserDto } from './dto/response/user.dto';
 
 @Injectable()
 export class UserService {
+  private readonly basicUserFileStorageSize = 1024 * 1024 * 1024 * 1; // 1GB
+  private readonly plusUserFileStorageSize = 1024 * 1024 * 1024 * 10; // 10GB
+  private readonly logger: Logger = new Logger(UserService.name);
+
   constructor(private readonly prisma: PrismaService) {}
 
   async findOne(uid: string): Promise<UserDto> {
@@ -48,5 +53,49 @@ export class UserService {
     }
 
     return new UserDto(user);
+  }
+
+  async getTotalFileSize(uid: string): Promise<bigint> {
+    const user = await this.prisma.user.findUnique({
+      where: { uid },
+    });
+
+    if (!user) {
+      throw new NotFoundException(`User with uid ${uid} not found`);
+    }
+
+    const documentFileSizeSummation = await this.prisma.document.aggregate({
+      where: { userId: user.id },
+      _sum: { fileSize: true },
+    });
+
+    this.logger.log(
+      `user ${uid} total file size: ${documentFileSizeSummation}`,
+    );
+
+    return documentFileSizeSummation._sum.fileSize;
+  }
+
+  async checkUploadAvailable(uid: string): Promise<boolean> {
+    const user = await this.prisma.user.findUnique({
+      where: { uid },
+    });
+
+    if (!user) {
+      throw new NotFoundException(`User with uid ${uid} not found`);
+    }
+
+    const totalFileSize = await this.getTotalFileSize(uid);
+
+    switch (user.role) {
+      case 'BASIC':
+        return totalFileSize < this.basicUserFileStorageSize;
+      case 'PLUS':
+        return totalFileSize < this.plusUserFileStorageSize;
+      case 'ADMIN':
+        return true;
+      default:
+        throw new Error(`Invalid role: ${user.role}`);
+    }
   }
 }
